a4163d4dcf48ff521148ffabf11a454efc9ed791
imsprbklpn <ulpan658@gmail.com>
2026-05-21 06:54:56 +0500

    fix: cleanup and refactor main application

--- src/main/java/com/saparbek/university/university/UniversityApplication.java
+++ src/main/java/com/saparbek/university/university/UniversityApplication.java
@@ -1,13 +1,20 @@
 package com.saparbek.university.university;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@OpenAPIDefinition(info = @Info(
+		title = "University Management System",
+		version = "1.0",
+		description = "REST API for University Management"
+))
 public class UniversityApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UniversityApplication.class, args);
 	}
-
 }
